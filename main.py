Fixed video recording and scaling issue

main.py
@@ -1,18 +1,18 @@
+from ctypes import windll
 from interaction_functions import *
 import cv2 as cv
 import numpy as np
 
 
-process_name = "HD-Player.exe"
-
-
 def main():
-    # Runs while BlueStacks is open
+    # Make program aware of DPI scaling,
+    windll.user32.SetProcessDPIAware()
+    hwnd = get_hwnd("downloads")
+    win32gui.SetForegroundWindow(hwnd)
     while True:
-    # while process_name in [p.name() for p in process_iter()]:
-        screenshot = get_screenshot()
+        screenshot = get_screenshot(hwnd)
         screenshot = np.array(screenshot)
-        screenshot = screenshot[:, :, ::-1].copy()
+        screenshot = cv.cvtColor(screenshot, cv.COLOR_RGB2BGR)
         cv.imshow("coc", screenshot)
 
         if cv.waitKey(1) == ord("q"):