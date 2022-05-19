243377a41492331e00fcde4692a0595e816cb103
Traptricker <85963782+Traptricker@users.noreply.github.com>
2022-05-18 23:42:53 -0400

    Image recognition of obstacles

--- main.py
+++ main.py
@@ -7,13 +7,55 @@ import numpy as np
 def main():
     # Make program aware of DPI scaling,
     windll.user32.SetProcessDPIAware()
-    hwnd = get_hwnd("downloads")
+    hwnd = get_hwnd("bluestacks")
     win32gui.SetForegroundWindow(hwnd)
+
+    obstacle_list = [
+        cv.imread("assets/obstacles/jpg/bush.jpg", cv.IMREAD_UNCHANGED),
+        cv.imread("assets/obstacles/jpg/large_tree.jpg", cv.IMREAD_UNCHANGED),
+        cv.imread("assets/obstacles/jpg/left_trunk.jpg", cv.IMREAD_UNCHANGED),
+        cv.imread("assets/obstacles/jpg/log.jpg", cv.IMREAD_UNCHANGED),
+        cv.imread("assets/obstacles/jpg/medium_tree.jpg", cv.IMREAD_UNCHANGED),
+        cv.imread("assets/obstacles/jpg/mushroom.jpg", cv.IMREAD_UNCHANGED),
+        cv.imread("assets/obstacles/jpg/right_trunk.jpg", cv.IMREAD_UNCHANGED),
+        cv.imread("assets/obstacles/jpg/small_tree.jpg", cv.IMREAD_UNCHANGED)
+    ]
+
+    threshold = 0.06
+
     while True:
         screenshot = get_screenshot(hwnd)
         screenshot = np.array(screenshot)
         screenshot = cv.cvtColor(screenshot, cv.COLOR_RGB2BGR)
-        cv.imshow("coc", screenshot)
+
+        for obstacle in obstacle_list:
+            result = cv.matchTemplate(screenshot, obstacle, cv.TM_SQDIFF_NORMED)
+            locations = np.where(result <= threshold)
+
+            # We can zip those up into a list of (x, y) position tuples
+            locations = list(zip(*locations[::-1]))
+            print(locations)
+
+            if locations:
+                print('Found needle.')
+
+                needle_w = obstacle.shape[1]
+                needle_h = obstacle.shape[0]
+                line_color = (0, 255, 0)
+                line_type = cv.LINE_4
+
+                # Loop over all the locations and draw their rectangle
+                for loc in locations:
+                    # Determine the box positions
+                    top_left = loc
+                    bottom_right = (top_left[0] + needle_w, top_left[1] + needle_h)
+                    # Draw the box
+                    cv.rectangle(screenshot, top_left, bottom_right, line_color, line_type)
+
+            else:
+                print('Needle not found.')
+
+        cv.imshow('Matches', screenshot)
 
         if cv.waitKey(1) == ord("q"):
             cv.destroyWindow()
